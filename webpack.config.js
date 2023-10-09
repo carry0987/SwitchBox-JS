8978307c72f6f68ef4df5254acc4ea2355c61aeb
carry0987 <carry0987@users.noreply.github.com>
2023-10-09 17:35:34 +0800

    Add CSS

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,37 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
 module.exports = {
     mode: 'production',
     context: path.resolve(__dirname, 'src'),
-    entry: {switchBox: './switchBox.js'},
+    entry: {
+        switchBox: ['./switchBox.js', './switchBox.css']
+    },
     output: {
         filename: '[name].min.js',
         path: path.resolve(__dirname, 'dist'),
         library: 'SwitchBox',
         libraryTarget: 'umd',
         globalObject: 'this'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader']
+            }
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: '[name].min.css'
+        })
+    ],
+    optimization: {
+        minimizer: [
+            `...`,
+            new CssMinimizerPlugin()
+        ]
     }
 };
